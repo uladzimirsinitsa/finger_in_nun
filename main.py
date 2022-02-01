d40948dc77307afc8c62b37ee1cc30c34464e629
Uladzimir Sinitsa <uladzimirsinitsa@gmail.com>
2022-02-02 01:34:15 +0300

    v0.1

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 
 import itertools
-import re
 
 from string import ascii_letters
 from string import digits
 from string import punctuation
 
+from datetime import datetime
+import time
+
+from win32com import client
+
 
 def brute_xlsx():
     possible_symbols = digits + ascii_letters + punctuation
-    for length in range(0,32):
+    count = 0
+    for length in range(32):
         for password in itertools.product(possible_symbols, repeat=length):
             password = ''.join(password)
             print(password)
+            open_doc = client.Dispatch('Excel.Application')
+            count += 1
+
+            try:
+                open_doc.Workbooks.Open(
+                    r'C:\Users\God\Desktop\test_password.xlsx',
+                    False,
+                    True,
+                    None,
+                    password
+                )
+                print(password)
+                return password
+            except:
+                print(count)
+
 
 
 
 def main():
+    start = time.time()
+    print(f'Started - {datetime.utcfromtimestamp(time.time()).strftime("%H:%M:%S")}')
     brute_xlsx()
+    print(f'Finished - {datetime.utcfromtimestamp(time.time()).strftime("%H:%M:%S")}')
 
 
 if __name__ == '__main__':
